fdaae0886cbcf8d2a017bd523f971b36cd737edd
momtinkao <momtinkao@gmail.com>
2022-03-02 10:55:19 +0800

    傳輸資料庫

--- app/src/main/java/com/example/oeientation/teacher.kt
+++ app/src/main/java/com/example/oeientation/teacher.kt
@@ -1,7 +1,16 @@
 package com.example.oeientation
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
+import android.provider.Settings
 import android.view.Menu
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AlertDialog
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.navigation.NavigationView
 import androidx.navigation.findNavController
@@ -11,7 +20,10 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.example.oeientation.databinding.ActivityTeacherBinding
+import com.example.oeientation.databinding.ContentTeacherBinding
 import com.example.oeientation.databinding.NavHeaderTeacherBinding
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.FirebaseFirestore
@@ -22,6 +34,11 @@ class teacher : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityTeacherBinding
+    private val launcherActivity = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()) {
+        val code = it.resultCode
+        val data = it.data
+    }
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -50,13 +67,16 @@ class teacher : AppCompatActivity() {
         val user = Firebase.auth.currentUser
         var uid = ""
         user?.let {
-            uid = user.uid
+            uid = user.displayName.toString()
         }
         header.textView.text = uid
 
 
 
     }
+
+
+
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -68,4 +88,76 @@ class teacher : AppCompatActivity() {
         val navController = findNavController(R.id.nav_host_fragment_content_teacher)
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
+
+    override fun onStart() {
+        super.onStart()
+        // 畫面開始時檢查權限
+        onClickRequestPermission()
+    }
+
+    private fun onAgree() {
+        Toast.makeText(this, "已取得定位權限", Toast.LENGTH_SHORT).show()
+        // 取得權限後要做的事情...
+    }
+
+    private fun onDisagree() {
+        Toast.makeText(this, "未取得定位權限", Toast.LENGTH_SHORT).show()
+        // 沒有取得權限的替代方案...
+    }
+
+    private val requestPermissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission())
+    { isGranted: Boolean ->
+        // 判斷使用者是否給予權限
+        if (isGranted) {
+            onAgree()
+        } else {
+            if (!ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
+                // 被拒絕太多次，無法開啟請求權限視窗
+                AlertDialog.Builder(this)
+                    .setTitle("需要定位權限")
+                    .setMessage("這個APP需要定位權限，因為被拒絕太多次，無法自動給予權限，請至設定手動開啟")
+                    .setPositiveButton("Ok") { _, _ ->
+                        // 開啟本App在設定中的權限視窗，在內心祈禱使用者願意給予權限
+                        openPermissionSettings()
+                    }
+                    .setNeutralButton("No") { _, _ -> onDisagree() }
+                    .show()
+            }
+        }
+    }
+
+    //取得權限
+    private fun onClickRequestPermission() {
+        when {
+            ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                    == PackageManager.PERMISSION_GRANTED -> {
+                // 情況一：已經同意
+                Toast.makeText(this, "已取得定位權限", Toast.LENGTH_SHORT).show()
+            }
+
+            ActivityCompat.shouldShowRequestPermissionRationale(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) -> {
+                // 情況二：被拒絕過，彈出視窗告知本App需要權限的原因
+                AlertDialog.Builder(this)
+                    .setTitle("需要定位權限")
+                    .setMessage("這個APP需要定位權限，請給予權限")
+                    .setPositiveButton("Ok") { _, _ -> requestPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION) }
+                    .setNeutralButton("No") { _, _ -> onDisagree() }
+                    .show()
+            }
+            else -> {
+                // 情況三：第一次請求權限，直接請求權限
+                requestPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+            }
+        }
+    }
+    private fun openPermissionSettings() {
+        val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+        val uri = Uri.fromParts("package", packageName, null)
+        intent.data = uri
+        startActivity(intent)
+    }
 }
+
--- app/src/main/java/com/example/oeientation/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/oeientation/ui/home/HomeFragment.kt
@@ -1,20 +1,39 @@
 package com.example.oeientation.ui.home
 
+import android.app.Activity
+import android.app.AlertDialog
+import android.content.Context.LOCATION_SERVICE
+import android.content.Intent
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
+import android.nfc.Tag
 import android.os.Bundle
+import android.provider.Settings
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.Button
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import com.example.oeientation.R
 import com.example.oeientation.databinding.FragmentHomeBinding
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.ktx.Firebase
+import com.google.type.LatLng
+
 
 class HomeFragment : Fragment() {
 
     private lateinit var homeViewModel: HomeViewModel
     private var _binding: FragmentHomeBinding? = null
+    var oriLocation:Location? =null
+    var flag = false
+    val locationList= mutableListOf<String>()
+
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -31,10 +50,25 @@ class HomeFragment : Fragment() {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
+
+        val locationManager = requireActivity().getSystemService(LOCATION_SERVICE) as LocationManager
+        val gps_location: Button = binding.gps
+        val stop_location:Button = binding.gps2
+        gps_location.setOnClickListener {
+            checkGPSState()
+            oriLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER)
+            Toast.makeText(activity,"按鈕緯度:${oriLocation!!.latitude} 按鈕經度:${oriLocation!!.longitude}",Toast.LENGTH_SHORT).show()
+            locationList.add("{${oriLocation!!.latitude},${oriLocation!!.longitude}}")
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+                3000L, 5f,locationListener)
+        }
+        stop_location.setOnClickListener{
+            val database = FirebaseDatabase.getInstance()
+            val myRef = database.getReference("users")
+            myRef.setValue(locationList)
+            locationManager.removeUpdates(locationListener);
+
+        }
         return root
     }
 
@@ -42,4 +76,45 @@ class HomeFragment : Fragment() {
         super.onDestroyView()
         _binding = null
     }
+
+
+    private val requestPermissionLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult())
+    { activityResult ->
+        // 判斷使用者是否給予權限
+        if (activityResult.resultCode == Activity.RESULT_OK) {
+            Toast.makeText(activity,"訂位已開啟", Toast.LENGTH_SHORT).show()
+        } else {
+            checkGPSState()
+        }
+    }
+
+    private fun checkGPSState() {
+        val locationManager = requireActivity().getSystemService(LOCATION_SERVICE) as LocationManager
+        if (!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            AlertDialog.Builder(activity)
+                .setTitle("GPS 尚未開啟")
+                .setMessage("使用此功能需要開啟 GSP 定位功能")
+                .setPositiveButton("前往開啟"){_, _ ->
+                    val intent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+                    requestPermissionLauncher.launch(intent)
+                    }
+                .setNeutralButton("取消", null)
+                .show()
+        } else {
+            //todo getDeviceLocation()
+            Toast.makeText(activity, "已獲取到位置權限且GPS已開啟，可以準備開始獲取經緯度", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    val locationListener = object : LocationListener{
+        override fun onLocationChanged(location: Location) {
+            if(location != null) {
+                Toast.makeText(activity,"緯度:${location.latitude} 經度:${location.longitude}",Toast.LENGTH_SHORT).show()
+                locationList.add("{${location.latitude},${location.longitude}}")
+            }
+
+        }
+
+    }
+
 }
